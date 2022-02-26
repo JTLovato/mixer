a4b2fe8b2b86da51c89497699b69e9311ab2cc24
John Lovato <jtlovato@gmail.com>
2022-02-26 14:28:34 -0700

    Added newData set

---
+++ src/NewData.js
@@ -0,0 +1,173 @@
+ingredients: [
+    {
+        alcohol: [
+            {
+                wine: [
+                    {
+                        red: [
+                            'Cabernet Sauvignon',
+                            'Pinot Noir',
+                            'Red Blends',
+                            'Malbec',
+                            'Merlot',
+                        ],
+                        white: [
+                            'Sauvignon Blanc',
+                            'Chardonnay',
+                            'Pinot Grigio',
+                            'Moscato',
+                            'Riesling',
+                        ],
+                        sparkling: [
+                            'Champagne',
+                            'Prosecco',
+                            'Amercian Sparkling',
+                            'Sparkling Rose',
+                        ],
+                        pink_wine: [
+                            'Rose',
+                            'White Zinfadel',
+                            'Pink Moscato',
+                        ],
+                        dessert_and_fortified: [
+                            'Port',
+                            'Dessert Wine',
+                            'Vermouth',
+                            'Sherry',
+                            'Madeira',
+                            'Ice Wine / Eiswein',
+                            'Marsala',
+                            'Pedro Zimenez',
+                        ]
+                    }
+                ],
+                spirits: [
+                    {
+                        whiskey: [
+                            'Bourbon',
+                            'Scotch Whisky',
+                            'Irish Whiskey',
+                            'American Whiskey',
+                            'Rye Whiskey',
+                            'Japanese Whisky',
+                            'Canadian Whisky',
+                            'Flavored Whiskey',
+                        ]
+                    },
+                    {
+                        tequila: [
+                            'Blanco',
+                            'Anejo',
+                            'Reposado Tequila',
+                            'Gold',
+                            'Flavored',
+                        ]
+                    },
+                    {
+                        gin: [
+                            'London Dry',
+                            'Modern',
+                            'Genever / Dutch',
+                            'Aged',
+                            'Old Tom',
+                            'Navy Strength',
+                        ]
+                    },
+                    {
+                        rum: [
+                            'Aged Rum',
+                            'Flavored Rum',
+                            'White Rum',
+                            'Spiced Rum',
+                            'Gold Rum',
+                            'Dark Rum',
+                        ]
+                    },
+                    {
+                        brandy: [
+                            'Cognac',
+                            'Armagnac',
+                            'Fruit Brandy',
+                            'Grappa',
+                            'Flavored Brandy',
+                            'Eau de Vie',
+                            'Calvados',
+                            'Pisco',
+                        ]
+                    },
+                    {
+                        liqueur: [
+                            'Fruit Liqueur',
+                            'Cream Liqueur',
+                            'Herbal Liqueur',
+                            'Triple Sec Liqueur',
+                            'Amaretto Liqueur',
+                            'Other Liqueur',
+                            'Chocolate Liqueur',
+                            'Coffee Liqueur',
+                            'Anise Liqueur',
+                            'Sloe Gin',
+                        ]
+                    },
+                    'Vodka',
+                    'Mezcal',
+
+                ],
+                beer: [
+                    'IPA',
+                    'Hard Seltzer',
+                    'Lager',
+                    'Light Beer',
+                    'Ale',
+                    'Ale',
+                    'Stout',
+                    'Sour Beer',
+                    'Belgian',
+                    'Cider',
+                    'Hazy IPA',
+                    'Non-Alcoholic',
+                ]
+            }
+        ],
+        ingredients: [
+            'lime juice',
+            'orange juice',
+            'lemon juice',
+            'orange juice',
+            'angostura',
+            'pineapple juice',
+            'apple juice',
+            'simple syrup',
+            'pomegranite juice',
+            'egg white',
+            'orange bitters',
+            'cranberry juice',
+            'club soda',
+            'soda',
+            'cream',
+            'grapefruit juice',
+            'milk',
+        ],
+        garnishes: [
+            'olives',
+            'maraschino cherries',
+            'limes',
+            'lemons',
+            'rosemary',
+            'salt',
+            'cinnamon',
+            'mint leaves',
+            'celery',
+            'cocktail onions',
+            'cucumbers',
+            'oranges',
+            'whipped cream',
+            'apples',
+            'bananas',
+            'candy',
+            'chocolate',
+            'pineapples',
+            'strawberries',
+        ]
+    }
+]
